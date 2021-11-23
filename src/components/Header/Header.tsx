6c4ef3846b91068367e29b822e3d1ec3c73c7e84
Старова Наталья <nstarova@htc-cs.ru>
2021-11-23 18:46:21 +0400

    feat: stylize  App, Header, added fonts

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.pcss";
-import { AppBar, Typography } from "@mui/material";
+import { AppBar, Link, Typography } from "@mui/material";
 import { Svg } from "../Svg/Svg";
 import { ISvgFile } from "../Svg";
 
@@ -10,11 +10,21 @@ export const Header = () => {
   return (
     <div className={"header"}>
       <AppBar color="transparent" position="static">
-        <Typography variant="h6" className={"title"}>
-          <section className={"section"}>
-            <Svg file={logo} />
-          </section>
-          News
+        <Typography variant="h6" className={"title"} paragraph={true}>
+          <Link href="#" color="inherit" variant="inherit" underline="none">
+            <div className={"section"}>
+              <Svg file={logo} />
+            </div>
+            <span>оdaerjay</span>
+          </Link>
+        </Typography>
+        <Typography variant="subtitle1" className={"title"} paragraph={true}>
+          <Link href="#" color="inherit" variant="inherit" underline="hover">
+            иллюстрации
+          </Link>
+          <Link href="#" color="inherit" variant="inherit" underline="hover">
+            кофейная карта
+          </Link>
         </Typography>
       </AppBar>
     </div>
